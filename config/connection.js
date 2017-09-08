59725287f66e3b29ebc9f5923f072d0500e8277b
Tiara Hamilton <tnhami63@gmail.com>
2017-09-08 09:44:03 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,14 @@
 var mysql = require("mysql");
 
-
-
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(porcess.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection ({
+        host: "localhost",
+        user: "root",
+        password: "hacktheplanet",
+        database: "todoagain_db"
+    });
+};
 
 module.exports = connection;
